141adc9db326b39553e3e532958af65c9175df16
Faisal <115137798+faisalhasan00@users.noreply.github.com>
2025-11-04 10:26:27 +0530

    Update HumanTalkWidget.tsx

--- client/src/components/HumanTalkWidget.tsx
+++ client/src/components/HumanTalkWidget.tsx
@@ -647,7 +647,7 @@ export const HumanTalkNavButton: React.FC = () => {
           d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
         />
       </svg>
-      <span className="inline whitespace-nowrap">Talk With Human</span>
+      <span className="inline whitespace-nowrap">Need Help? Talk to a Real Person</span>
       <div className="absolute -top-1 -right-1 rounded-full bg-red-500 px-1 py-0.5 text-[8px] font-bold uppercase text-white shadow-sm">
         Beta
       </div>
